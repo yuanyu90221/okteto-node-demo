149752719d35b7dbd438f5db802b91c788abf332
yuanyu90221 <yuanyu90221@gmail.com>
2020-02-12 15:54:27 +0800

    update port

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const http = require('http');
 
 const hostname = '0.0.0.0';
-const port = 3000;
+const port = 3300;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(`Hello world!`);
+  res.end(`Hello world from the cluster!`);
 });
 
 server.listen(port, hostname, () => {
